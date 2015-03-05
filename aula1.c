54894a68b41da7a706e26443a7da64e995579a22
Leonso <lelorado@gmail.com>
2015-03-05 12:15:45 -0300

    Alterei

---
+++ aula1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+	int matricula;
+	char nome[100];
+	printf("Digite Matrícula: ");
+	scanf("%d", &matricula);
+	printf("Digite Nome: ");
+	scanf("%s", &nome);
+	f = fopen("saida.txt", "w");
+	fprint(f, "%d%s\n", matricula, nome");
+	fclose(f);
+}
