db83bbf707ff5917f4cbef2cfa1cbaf615199f28
LeonsoLeitao <lelorado@gmail.com>
2015-03-15 23:25:28 -0300

    Alterei again

--- T1/t1.c
+++ T1/t1.c
@@ -9,20 +9,20 @@ void le_alunos(int* matriculas, char nomes[][50], int* n){
     char nome[50];
     int i;
     int linha;
-    FILE*f = fopen("alunos.txt", "R");
+    FILE*f = fopen("alunos.txt", "r");
     linha = 0;
     while(feof(f) == 0){
         if(fscanf(f, "%d", &mat) <= 0)
             break;
         i = 0;
         c = fgetc(f);
-        while(c != '\n'){
+        while(c != '\n' && feof(f) == 0){
             nome[i] = c;
             c = fgetc(f);
             i++;
         }
         nome[i] = '\0';
-        matriculas[linha]=mat;
+        matriculas[linha] = mat;
         strcpy(nomes[linha], nome);
         linha++;
     }
@@ -33,15 +33,23 @@ void le_alunos(int* matriculas, char nomes[][50], int* n){
 void leitor_nota(float* media){
     int i = 0, mat;
     float n1, n2;
-    FILE*f = fopen("alunos.txt", "R");
+    FILE*f = fopen("notas.txt", "r");
     while(feof(f) == 0){
-        if(fscanf(f, "%d %f %f\n", &mat, &n1, &n2) == 0)
+        if(fscanf(f, "%d %f %f\n", &mat, &n1, &n2) <= 0)
             break;
         media[i] = (n1+n2)/2;
         i++;
     }
     fclose(f);
+}
 
+void procura(char* nome, char nomes[][50], int n, float* media){
+    int i;
+    for(i = 0; i<n; i++){
+        if(strstr(nomes[i], nome) != NULL){
+            printf("%.2f %s\n", media, nomes);
+        }
+    }
 }
 
 int main(int argc, char** argv){
@@ -56,5 +64,7 @@ int main(int argc, char** argv){
     int n;
     le_alunos(matriculas, nomes, &n);
     leitor_nota(med);
+    procura(nome, nomes, n, med);
 
+    return (0);
 }
